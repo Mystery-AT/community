0aa924f83f11dbfd7d1dbd51596c5863492a416a
lrj <mystery365699552@gmail.com>
2020-03-30 22:23:58 +0800

    第一次

---
+++ src/test/java/com/lrj/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrj.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
