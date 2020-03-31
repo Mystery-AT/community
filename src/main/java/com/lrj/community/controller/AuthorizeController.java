a8b86151f62db4157ff162333c2e56fc7ca5d05a
lrj <mystery365699552@gmail.com>
2020-03-31 22:22:59 +0800

    json数据

---
+++ src/main/java/com/lrj/community/controller/AuthorizeController.java
@@ -0,0 +1,29 @@
+package com.lrj.community.controller;
+
+import com.lrj.community.dto.AccessTokenDTO;
+import com.lrj.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code, @RequestParam("state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("b7af8716ce883057c670");
+        accessTokenDTO.setClient_secret("c1a5403084af4a43dd70767b2e65297475c56385");
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "index";
+    }
+
+
+}
