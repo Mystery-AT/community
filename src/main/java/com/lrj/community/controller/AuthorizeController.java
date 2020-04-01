94c8bc831246e741a963aecfbb737f2b3e83ced2
lrj <mystery365699552@gmail.com>
2020-04-01 20:13:09 +0800

    变量抽离到yml文件

--- src/main/java/com/lrj/community/controller/AuthorizeController.java
+++ src/main/java/com/lrj/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lrj.community.dto.AccessTokenDTO;
 import com.lrj.community.dto.GithubUser;
 import com.lrj.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +15,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code, @RequestParam("state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("b7af8716ce883057c670");
-        accessTokenDTO.setClient_secret("c1a5403084af4a43dd70767b2e65297475c56385");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getGithubUser(accessToken);
         System.out.println(user.getName());
+        System.out.println(user);
         return "index";
     }
 
